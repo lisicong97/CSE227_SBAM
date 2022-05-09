4836dd7f89e9e0f6380c65e854782f65c8e48237
Jiaqi Yang <jyang62@buffalo.edu>
2022-05-09 12:44:19 -0700

    init register user

--- sbam.py
+++ sbam.py
@@ -1,26 +1,55 @@
-import rsa
+from Crypto.PublicKey import RSA
 import sys
+import requests
+from hashlib import sha512
+import json
 
 # sbam message_name option1 option2...
 if len(sys.argv) == 1:
-	print 'Please type your command'
+	print("Please type your command")
 
 message = sys.argv[1]
 
 # sbam new-user userName
 if message == 'new-user':
-	(pk, sk) = rsa.newkeys(2048)
+	if len(sys.argv) < 3:
+		print("Please type in your user name")
+
+	userName = sys.argv[2]
+	keyPair = RSA.generate(bits=1024)
+
 	# save the public key
-	publicKey = pk.save_pkcs1()
-	pubFile = open('public_key.key', 'wb')
+	publicKey = keyPair.publickey().export_key()
+	pubFile = open("publicKey.pem", "wb")
 	pubFile.write(publicKey)
 	pubFile.close()
 
 	# save the private key 
-	privateKey = sk.save_pkcs1()
-	priFile = open('private_key.key', 'wb')
+	privateKey = keyPair.export_key()
+	priFile = open("privateKey.pem", "wb")
 	priFile.write(privateKey)
 	priFile.close()
+
+	# send userName to request sign message from server
+	userInfo = {'userName': userName, 'publicKey': {'e': keyPair.e, 'n': keyPair.n}}
+	data = json.dumps(userInfo)
+	response1 = requests.post("/registerUser", data=data)
+
+    # deal with the message from the server
+	if response1['ifSuccess'] == False:
+		print("User Name Has Been Taken!")
+	else:
+		msg = response1.text['msg']
+		hash = int.from_bytes(sha512(msg).digest(), byteorder='big')
+		signature = pow(hash, keyPair.d, keyPair.n)
+		signInfo = {'userName': userName, 'signedMsg': signature}
+		data = json.dumps(signInfo)
+		response2 = requests.post("/registerUserConfirm", data=data)
+		if response2['ifSuccess'] == False:
+			print("Register Failed!")
+		else:
+			print("Register Success!")
+
 
 # sbam new-pkg userName pkgName
 if message == 'new-pkg':
