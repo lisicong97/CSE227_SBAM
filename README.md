e55570efd2024448ae96fcdff6501a210551d51f
lisicong <lisicong97@gmail.com>
2022-05-17 11:47:05 -0700

    blc

--- README.md
+++ README.md
@@ -2,6 +2,17 @@
 
 **a Secure Package Manager based in block-chain based on SPAM**
 
+Use geth to run block chain locally,  and rewrite configuration in proj/truffle-config.js to run Sbam smart contract. Below is an example:
+
+```
+geth --port 3000 --networkid 58343 --nodiscover --datadir=./blkchain --maxpeers=0  --rpc --rpcport 8543 --rpcaddr 127.0.0.1 --rpccorsdomain "*" --rpcapi "eth,net,web3,personal,miner"
+geth attach http://127.0.0.1:8543
+cd proj
+truffle migrate
+```
+
+
+
 To start the server, using command below:
 
 ```
@@ -13,7 +24,6 @@ To interact with server, using command below:
 ```
 python sbam.py {paras}
 ```
-
 
 Update 4.24
 
@@ -28,3 +38,7 @@ Update 5.3
 
 Update 5.12
 * finished server-client structure according to above doc
+
+Update 5.17
+
+* finished ethereum coding, smart constract is in Sbam.sol
--- proj/contracts/Sbam.sol
+++ proj/contracts/Sbam.sol
@@ -1,49 +1,62 @@
 pragma solidity >=0.4.25;
 
-struct User {
+contract Sbam {
+
+    struct User {
     uint userId;
     string userName;
     bytes[] publicKey;
-}
-
-struct Package {
-	uint packageId;
-	string packageName;
-	bytes[][] publicKey; // may have different collaborators
-    uint version;
-	bytes[][] content;
-}
-
-contract Sbam {
-    mapping (string => uint) userName2userId;
-    mapping (uint => User) userId2user;
-
-    mapping(string => uint) pkgName2pkgId;
-	mapping(uint => Package) pkgId2pkg;
-
-    uint nextUserId;
-
-    constructor() {
-        nextUserId = 1;
+    string socialMedia;
     }
+
+    struct Package {
+        string pkgName;
+        string owner;
+        bytes[] signature; // sign by uploader's pkg private key
+    }
+
+    mapping (string => User) userName2user;
+    mapping (string => Package) pkgName2pkg;
 
     event printToConsole(string message);
 
-    function registerUser(string memory userName) public returns(bool) {
-        // already has user name
-        if (userName2userId[userName] != 0) {
+    // after register to server, register to block chain
+    function registerUser(uint id, string memory userName, bytes[] memory publicKey, 
+        string memory socialMedia) public returns(bool) {
+        if (keccak256(abi.encodePacked(userName2user[userName].userName)) 
+            != keccak256(abi.encodePacked(""))) {
             emit printToConsole("This user name is occupied, please try another one.");
             return false;
         } else {
-            userName2userId[userName] = nextUserId;
-            nextUserId++;
-            // TODO: generate key pair
-            emit printToConsole("IMPORTANT: your private key is 111");
+            User memory user = User(id, userName, publicKey, socialMedia);
+            userName2user[userName] = user;
             return true;
         }
     }
 
-    function getUserIdByName(string memory userName) public view returns(uint) {
-        return userName2userId[userName];
+    function getUser(string memory userName) public view returns(User memory) {
+        return userName2user[userName]; // if value is empty, the return object will have default value ("", 0, balabala)
+    }
+
+    // pkg content sign by private key of pkg
+    function registerPkg(string memory pkgName, string memory ownername, bytes[] memory sign) public returns(bool) {
+        if (keccak256(abi.encodePacked(pkgName2pkg[pkgName].pkgName)) 
+            != keccak256(abi.encodePacked(""))) {
+            emit printToConsole("This pkg name is occupied, please try another one.");
+            return false;
+        } else {
+            Package memory pkg = Package(pkgName, ownername, sign);
+            pkgName2pkg[pkgName] = pkg;
+            return true;
+        }
+    }
+
+    function updatePkg(string memory pkgName, bytes[] memory sign) public returns(bool) {
+        pkgName2pkg[pkgName].signature = sign;
+        return true;
+    }
+
+    function getPkg(string memory pkgName)  public view returns(Package memory) {
+        return pkgName2pkg[pkgName];
     }
 }
