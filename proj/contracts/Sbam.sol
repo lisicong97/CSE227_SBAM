b17528862ddbe15b7f09c4e99610ee543af65e9f
lisicong <lisicong97@gmail.com>
2022-05-30 16:55:23 -0700

    add sign of para

--- proj/contracts/Sbam.sol
+++ proj/contracts/Sbam.sol
@@ -64,13 +64,17 @@ contract Sbam {
         string memory pkgName,
         string memory version,
         string memory updater,
-        string memory sign
+        string memory sign,
+        string memory userSignOf_pkgName_version_updater
     ) public returns (bool) {
         // if (msg.sender != authenticatedServerAddress) {
         //     emit printToConsole("Only authticated server address can write data to block chain!");
         //     return false;
         // }
-
+        // if (!checkUserSign(userName2user[updater].publicKey, userSignOf_pkgName_version_updater, string(abi.encodePacked(pkgName, version, updater)))) {
+        //     emit printToConsole("Only authticated user can write data to block chain!");
+        //     return false;
+        // }
         string memory key = string(abi.encodePacked(pkgName, "_", version));
         if (
             keccak256(abi.encodePacked(pkgName2pkg[key].pkgName)) !=
@@ -101,10 +105,18 @@ contract Sbam {
         return pkgName2pkg[key];
     }
 
-    function addPkgOwner(string memory ownerName, string memory pkgPubKey, string memory pkgName)
+    function addPkgOwner(string memory ownerName, string memory pkgPubKey, string memory pkgName, string memory userSignOf_ownerName_pkgName)
         public
         returns (bool)
     {
+        // if (msg.sender != authenticatedServerAddress) {
+        //     emit printToConsole("Only authticated server address can write data to block chain!");
+        //     return false;
+        // }
+        // if (!checkUserSign(userName2user[ownerName].publicKey, userSignOf_ownerName_pkgName, string(abi.encodePacked(ownerName, pkgName)))) {
+        //     emit printToConsole("Only authticated user can write data to block chain!");
+        //     return false;
+        // }
         if (
             keccak256(
                 abi.encodePacked(pkgName2Collaborators[pkgName].ownerName)
@@ -129,10 +141,18 @@ contract Sbam {
         }
     }
 
-    function addPkgCollaborator(string memory colName, string memory pkgPubKey, string memory pkgName)
+    function addPkgCollaborator(string memory colName, string memory pkgPubKey, string memory pkgName, string memory userSignOf_colName_pkgName)
         public
         returns (bool)
     {
+        // if (msg.sender != authenticatedServerAddress) {
+        //     emit printToConsole("Only authticated server address can write data to block chain!");
+        //     return false;
+        // }
+        // if (!checkUserSign(userName2user[ownerName].publicKey, userSignOf_colName_pkgName, string(abi.encodePacked(colName, pkgName)))) {
+        //     emit printToConsole("Only authticated user can write data to block chain!");
+        //     return false;
+        // }
         if (
             keccak256(
                 abi.encodePacked(pkgName2Collaborators[pkgName].ownerName)
@@ -174,5 +194,9 @@ contract Sbam {
         returns (PkgCollaborator memory)
     {
         return pkgName2Collaborators[pkgName];
+    }
+
+    function checkUserSign(string memory pubKey, string memory sign, string memory para) public pure returns (bool) {
+        return true;
     }
 }
